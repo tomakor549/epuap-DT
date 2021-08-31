8c2d8e29a01c93e2aa4ae432804cde6ca6e83cd9
tomakor549 <venetor7@gmail.com>
2021-08-31 15:36:34 +0200

    AuthRequest test create

--- ePUAP_DT/Models/AuthnRequest.cs
+++ ePUAP_DT/Models/AuthnRequest.cs
@@ -3,10 +3,119 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ePUAP_DT.Models
 {
-    class AuthnRequest
+    /// <summary>
+    /// SAML2 AuthnRequest model
+    /// </summary>
+    [XmlRoot("AuthnRequest", Namespace = "urn:oasis:names:tc:SAML:2.0:protocol")]
+    public class AuthnRequest
     {
+
+        public AuthnRequest()
+        {
+            this.IssueInstant = DateTime.Now;
+            this.NameIDPolicy = new NameIDPolicy();
+            this.RequestedAuthnContext = new RequestAuthContext();
+        }
+
+        private XmlSerializerNamespaces _xmlns;
+
+        [XmlNamespaceDeclarations]
+        public XmlSerializerNamespaces Xmlns
+        {
+            get
+            {
+                if (_xmlns == null)
+                {
+                    _xmlns = new XmlSerializerNamespaces();
+                    _xmlns.Add("saml2p", "urn: oasis:names: tc:SAML: 2.0:protocol");
+                }
+                return _xmlns;
+            }
+
+            set
+            {
+                _xmlns = value;
+            }
+        }
+
+        [XmlAttribute("ID")]
+        public string ID { get; set; }
+
+        [XmlAttribute("Version")]
+        public string Version { get; set; }
+
+        [XmlAttribute("IssueInstant")]
+        public DateTime IssueInstant { get; set; }
+
+        [XmlAttribute("AssertionConsumerServiceURL")]
+        public string AssertionConsumerServiceURL { get; set; }
+
+        [XmlAttribute("Destination")]
+        public string Destination { get; set; }
+
+        [XmlAttribute("ProtocolBinding")]
+        public string ProtocolBinding { get; set; }
+
+        [XmlElement("Issuer", Namespace = "urn:oasis:names:tc:SAML:2.0:assertion")]
+        public string Issuer { get; set; }
+
+        [XmlElement("NameIDPolicy", Namespace = "urn:oasis:names:tc:SAML:2.0:protocol")]
+        public NameIDPolicy NameIDPolicy { get; set; }
+
+        [XmlElement("RequestedAuthnContext", Namespace = "urn:oasis:names:tc:SAML:2.0:protocol")]
+        public RequestAuthContext RequestedAuthnContext { get; set; }
+
+    }
+
+    public class NameIDPolicy
+    {
+        public NameIDPolicy()
+        {
+            this.Format = NameID.UNSPECIFIED;
+        }
+
+        [XmlAttribute("AllowCreate")]
+        public bool AllowCreate { get; set; }
+
+        [XmlAttribute("Format")]
+        public string Format { get; set; }
+
+    }
+
+    /// <summary>
+    /// NameID values
+    /// </summary>
+    public class NameID
+    {
+        public const string UNSPECIFIED = "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified";
+        public const string PROVIDER = "urn:oasis:names:tc:SAML:2.0:nameid-format:provider";
+        public const string FEDERATED = "urn:oasis:names:tc:SAML:2.0:nameid-format:federated";
+        public const string TRANSIENT = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient";
+        public const string PERSISTENT = "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent";
+    }
+
+    public class RequestAuthContext
+    {
+        [XmlAttribute("Comparison")]
+        public AuthnContextComparisonType Comparison { get; set; }
+
+        [XmlElement("AuthnContextClassRef", Namespace = "urn:oasis:names:tc:SAML:2.0:assertion")]
+        public virtual string AuthnContextClassRef { get; set; }
+    }
+
+    public enum AuthnContextComparisonType
+    {
+        [XmlEnum("exact")]
+        Exact,
+        [XmlEnum("mininum")]
+        Minimum,
+        [XmlEnum("maximum")]
+        Maximum,
+        [XmlEnum("better")]
+        Better,
     }
 }
--- ePUAP_DT/Program.cs
+++ ePUAP_DT/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ePUAP_DT.Models;
 
 namespace ePUAP_DT
 {
@@ -10,6 +13,13 @@ namespace ePUAP_DT
     {
         static void Main(string[] args)
         {
+            AuthnRequest request = new AuthnRequest();
+
+            XmlSerializer mySerializer = new XmlSerializer(typeof(AuthnRequest));
+
+            StreamWriter myWriter = new StreamWriter("myFileName.xml");
+            mySerializer.Serialize(myWriter, request);
+            myWriter.Close();
         }
     }
 }
